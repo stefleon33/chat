163c91f1451cb596c591cf6eb77f51240917d3e9
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-11 17:32:20 -0400

    Background image set

--- components/Start.js
+++ components/Start.js
@@ -4,8 +4,14 @@ import { StyleSheet, View, Text, TextInput, ImageBackground, TouchableOpacity } 
 const Screen1 = ({ navigation }) => {
   const [name, setName] = useState('');
 
+  const [backgroundColor, setBackgroundColor] = useState("#090C08");
  return (
    <View style={styles.container}>
+    <ImageBackground 
+      source={require("../assets/backgroundImage.png")} 
+      style={styles.background}
+    >
+    </ImageBackground>
    </View>
  );
 }
@@ -17,4 +23,12 @@ const styles = StyleSheet.create({
    alignItems: 'center',
  },
 
+ background: {
+  flex: 1,
+  resizeMode: "cover",
+  height: '100%',
+  width: '100%',
+  alignItems: 'center',
+  justifyContent: 'center', 
+ },
 export default Screen1;
