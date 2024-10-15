38d0291eed39be4c83be58a94158f9fe223ae019
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-14 22:23:30 -0400

    Updated system message

--- components/Chat.js
+++ components/Chat.js
@@ -21,7 +21,7 @@ const Chat = ({ route, navigation }) => {
       },
       {
         _id: 2,
-        text: 'This is a system message',
+        text: 'Start chatting!',
         createdAt: new Date(),
         system: true,
       },
