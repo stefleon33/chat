65b905070b4b20989784af9b74ffab7857e74d4c
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-14 22:07:10 -0400

    moved location of imports

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
+// import the screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// import the screens
-import Screen1 from './components/Start';
-import ChatScreen from './components/Chat';
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
