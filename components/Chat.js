23076e6ac02fa900bdcc60f3dd6ad504629fc9ba
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-14 22:10:40 -0400

    Updated useEffect to include setMessages

--- components/Chat.js
+++ components/Chat.js
@@ -6,8 +6,26 @@ const Chat = ({ route, navigation }) => {
   const { name, backgroundColor } = route.params;
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
+    useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
 
  return (
