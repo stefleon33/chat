5c4045ca591458f5c727dd9b4b2c7afb99ba3cee
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-23 14:27:43 -0400

    updated imports from firebase/storage

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,11 +1,8 @@
 import { TouchableOpacity, Text, View, StyleSheet, Alert } from "react-native";
 import { useActionSheet } from '@expo/react-native-action-sheet';
-
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import * as ImagePicker from 'expo-image-picker';
-import * as MediaLibrary from 'expo-media-library';
-
 import * as Location from 'expo-location';
-import { ref, uploadBytes } from 'firebase/storage';
 
 const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage }) => {
     const actionSheet = useActionSheet();
