5826969f0dc62f69bb0a33e1f526f6385d0cc2b8
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-23 10:57:16 -0400

    OnActionPress button defined

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -5,3 +5,26 @@ import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 
 import * as Location from 'expo-location';
+    const onActionPress = () => {
+        const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
+        const cancelButtonIndex = options.length - 1;
+        actionSheet.showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex,
+            },
+            async (buttonIndex) => {
+                switch (buttonIndex) {
+                    case 0:
+                        pickImage();
+                        return;
+                    case 1:
+                        takePhoto();
+                        return;
+                    case 2:
+                        getLocation();
+                    default:
+                }
+            },
+        );
+    };
