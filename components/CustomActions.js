7b2ae824eff78d13bdac0fa97fb1e6e45885fc0a
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-23 10:58:36 -0400

    StylesSheet

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -79,3 +79,26 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: '#b2b2b2',
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: '#b2b2b2',
+    fontWeight: 'bold',
+    fontSize: 10,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+  },
+});
+
+export default CustomActions;
