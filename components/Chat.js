b48f8a36e5920c55c4dc23fffb0f2cc4f8fb1c46
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-23 10:55:46 -0400

    renderCustomView written and passed as a prop

--- components/Chat.js
+++ components/Chat.js
@@ -94,6 +94,27 @@ const Chat = ({ route, navigation, db, isConnected }) => {
     return <CustomActions {...props} />;
   };
 
+  /* Checks if the current message contains location data */
+  const renderCustomView = (props) => {
+    const { currentMessage} = props;
+    if (currentMessage.location) {
+      return (
+          <MapView
+            style={{width: 150,
+              height: 100,
+              borderRadius: 13,
+              margin: 3}}
+            region={{
+              latitude: currentMessage.location.latitude,
+              longitude: currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+      );
+    }
+    return null;
+  }
 
 /* Renders background color, messages and keyboard adjustments */
  return (
@@ -108,6 +129,7 @@ const Chat = ({ route, navigation, db, isConnected }) => {
       renderInputToolbar={renderInputToolbar}
       onSend={messages => onSend(messages)}
       renderActions={renderCustomActions}
+      renderCustomView={renderCustomView}
       user={{
         _id: userID,
         name: name,
